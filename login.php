8afa3a0a62dfeeccbc405daa9863676e58c05c81
chinoantraxx57 <hu18101054@lahuerta.tecmm.edu.mx>
2022-11-30 21:45:59 -0600

    login2

--- login.php
+++ login.php
@@ -1,43 +1,49 @@
 <?php session_start() ?>
-<!-- <div class="container-fluid">
+<div class="container-fluid">
 	<form action="" id="login-frm">
 		<div class="form-group">
-			<label for="" class="control-label">Correo</label>
-			<input type="email" name="email" required="" class="form-control">
+			<label for="" class="control-label">Usuario</label>
+			<input type="text" name="user" required="" class="form-control">
 		</div>
 		<div class="form-group">
 			<label for="" class="control-label">Contraseña</label>
 			<input type="password" name="password" required="" class="form-control">
 			<small><a href="javascript:void(0)" id="new_account">Nueva cuenta</a></small>
 		</div>
-		<button class="button btn btn-info btn-sm">Entrar</button>
+		<button class="button btn btn-warning  btn-sm">Entrar</button>
 	</form>
-</div> -->
-<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Login</legend>
-<input type='hidden' name='submitted' id='submitted' value='1'/>
-
-<label for='username' >UserName*:</label>
-<input type='text' name='username' id='username'  maxlength="50" />
-
-<label for='password' >Password*:</label>
-<input type='password' name='password' id='password' maxlength="50" />
-<button class="button btn btn-info btn-sm">Entrar</button>
-
-
-</fieldset>
-</form>
+</div>
 <style>
 	#uni_modal .modal-footer{
 		display:none;
 	}
 	.button {
-		background-color: #4CAF50;
-		border-radius: 8px;
-		font-size: 20px;
-		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)
-		} /* Red */ 
+		color: #212529;
+  background-color: #ffc107;
+  border-color: #ffc107;
+		} 
+		.container-fluid{
+			width: 50%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.form-control {
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
 </style>
 
 <script>
